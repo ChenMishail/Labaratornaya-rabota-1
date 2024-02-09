a1aeccbf60dafab7d1db22a13d88de9f73c2fa16
ChenMishail <mishadch05@gmail.com>
2024-02-09 23:36:43 +0200

    Res1

---
+++ Res1/Version 1/Version 1.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+using namespace std;
+void main()
+{
+	setlocale(0, "");
+	//4 вариант
+
+	float z;// z мин значение -123456792.0000000 мах значение 123456792.000000
+	unsigned short x, y; // x,y мин значение -32768 максимальное 32367 
+	cout << "Введите два катета прямоугольного треугольника\n";
+	cin >> x >> y;
+	z = sqrt((float)x * x + (float)y * y);
+	cout << "Гипотенуза равна\n" << z;
+}
